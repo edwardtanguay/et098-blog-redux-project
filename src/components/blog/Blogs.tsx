6480831c0cc9b0e485ce80891e619b1c00d9f05a
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-28 15:36:56 +0200

    fixed typescript error

--- src/components/blog/Blogs.tsx
+++ src/components/blog/Blogs.tsx
@@ -1,6 +1,6 @@
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {NavLink} from "react-router-dom";
-import {AppDispatch, RootState} from "../../store";
+import {RootState} from "../../store";
 import SearchBox from "../SearchBox";
 import ShowTime from "../ShowTime";
 import ShowAuthor from "../ShowAuthor";
